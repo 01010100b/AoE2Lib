5eb288164008b29cd2448e7d90914ae82f843446
01010100b <caveman1917@hotmail.com>
2020-10-26 23:56:52 +0100

    add unary

---
+++ Unary/Injector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unary
+{
+    public static class Injector
+    {
+        public static void Inject(Process process, string dll)
+        {
+
+        }
+    }
+}
