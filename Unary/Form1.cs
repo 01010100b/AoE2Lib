a28bd7e75b251095b18249c01e23ca97ea19b90c
01010100b <caveman1917@hotmail.com>
2020-10-27 00:46:16 +0100

    add proto

--- Unary/Form1.cs
+++ Unary/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Reloaded.Injector;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,20 @@ namespace Unary
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ButtonTest_Click(object sender, EventArgs e)
+        {
+            ButtonTest.Enabled = false;
+            Refresh();
+
+            var dll = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "aimodule.dll");
+            var process = Process.GetProcessesByName("AoE2DE_s")[0];
+
+            using (var injector = new Injector(process))
+            {
+                injector.Inject(dll);
+            }
         }
     }
 }
