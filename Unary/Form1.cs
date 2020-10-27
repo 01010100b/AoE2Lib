5f2a6303b16a08628604c6b94aa4a09b6f1f2c7a
01010100b <caveman1917@hotmail.com>
2020-10-27 01:47:00 +0100

    m

--- Unary/Form1.cs
+++ Unary/Form1.cs
@@ -1,4 +1,11 @@
-﻿using Reloaded.Injector;
+﻿using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Protos;
+using Protos.Expert;
+using Protos.Expert.Action;
+using Protos.Expert.Fact;
+using Reloaded.Injector;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,8 +15,11 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Protos.AIModuleAPI;
+using static Protos.Expert.ExpertAPI;
 
 namespace Unary
 {
@@ -32,6 +42,28 @@ namespace Unary
             {
                 injector.Inject(dll);
             }
+
+            Thread.Sleep(3000);
+
+            var channel = new Channel("localhost:37412", ChannelCredentials.Insecure);
+            var module_api = new AIModuleAPIClient(channel);
+            var expert_api = new ExpertAPIClient(channel);
+
+            var commands = new List<IMessage>()
+            {
+                new UpGetFact() { FactId = 25, FactParam = 83, GoalId = 99 },
+                new Goal() { GoalId = 99 }
+            };
+
+            var commandlist = new CommandList();
+            commandlist.PlayerNumber = 1;
+            foreach (var command in commands)
+            {
+                commandlist.Commands.Add(Any.Pack(command));
+            }
+
+            var resultlist = expert_api.ExecuteCommandList(commandlist);
+            Debug.WriteLine(resultlist);
         }
     }
 }
