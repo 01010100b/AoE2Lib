3dfe334b5fdc136ce3feaf93e9aeda6b710d2023
01010100b <caveman1917@hotmail.com>
2021-03-08 15:09:07 +0100

    remove msgpack-rpc dependency as have own implementation

--- MsgPack.Rpc.Core/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Security;
-
-[assembly: SecurityRules(SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
-[assembly: AllowPartiallyTrustedCallers]
--- MsgPack.Rpc.Core/Rpc/ExceptionModifiers.cs
+++
@@ -1,5 +0,0 @@
-namespace MsgPack.Rpc.Core {
-	internal static class ExceptionModifiers {
-		public static readonly object IsMatrioshkaInner = string.Intern(typeof(ExceptionModifiers).FullName + ".IsMatrioshkaInner");
-	}
-}
