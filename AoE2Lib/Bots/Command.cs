6e9cc92262567155169900a4a3ef1257fa8ef028
01010100b <caveman1917@hotmail.com>
2021-03-18 17:26:36 +0100

    Add support for conditional commands

--- AoE2Lib/Bots/Command.cs
+++ AoE2Lib/Bots/Command.cs
@@ -1,6 +1,7 @@
 ﻿using AoE2Lib.Utils;
 using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
+using Protos.Expert;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -24,9 +25,30 @@ namespace AoE2Lib.Bots
             Responses.Clear();
         }
 
-        public void Add(IMessage message)
+        public void Add(IMessage command)
         {
-            Messages.Add(message);
+            Messages.Add(command);
+        }
+
+        public void Add(params IMessage[] commands)
+        {
+            Messages.AddRange(commands);
+        }
+
+        public void Add(IMessage fact, string op, int value, params IMessage[] commands)
+        {
+            foreach (var command in commands)
+            {
+                var cc = new ConditionalCommand()
+                {
+                    Fact = Any.Pack(fact),
+                    CompareOp = op,
+                    Value = value,
+                    Command = Any.Pack(command)
+                };
+
+                Messages.Add(cc);
+            }
         }
 
         public IReadOnlyList<Any> GetResponses()
