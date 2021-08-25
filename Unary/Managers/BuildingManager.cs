d83219ed5d809540aa1bfca5d51e2186890bbf29
01010100b <caveman1917@hotmail.com>
2021-08-25 21:52:05 +0200

    remove Operations

--- Unary/Managers/BuildingManager.cs
+++ Unary/Managers/BuildingManager.cs
@@ -4,14 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Unary.Operations;
 
 namespace Unary.Managers
 {
     class BuildingManager : Manager
     {
-        private BuildOperation BuildOperation { get; set; }
-
         public BuildingManager(Unary unary) : base(unary)
         {
 
@@ -64,66 +61,6 @@ namespace Unary.Managers
             if (Unary.GameState.MyPlayer.GetFact(FactId.POPULATION_HEADROOM) > 0 && Unary.GameState.MyPlayer.GetFact(FactId.HOUSING_HEADROOM) < margin && house.Pending < pending)
             {
                 house.Build(1000, pending, (int)Priority.HOUSING);
-            }
-        }
-
-        private void DoBuildingOperations()
-        {
-            if (BuildOperation == null)
-            {
-                BuildOperation = new BuildOperation(Unary.OperationsManager);
-            }
-
-            var free_foundations = new HashSet<Unit>();
-            foreach (var unit in Unary.GameState.GetAllUnits())
-            {
-                if (unit.PlayerNumber != Unary.PlayerNumber)
-                {
-                    continue;
-                }
-
-                if (unit.Targetable == false || unit.Updated == false)
-                {
-                    continue;
-                }
-
-                if (unit[ObjectData.STATUS] != 0)
-                {
-                    continue;
-                }
-
-                if (unit[ObjectData.CMDID] != (int)CmdId.CIVILIAN_BUILDING && unit[ObjectData.CMDID] != (int)CmdId.MILITARY_BUILDING)
-                {
-                    continue;
-                }
-
-                free_foundations.Add(unit);
-            }
-
-            foreach (var build in Unary.OperationsManager.Operations.OfType<BuildOperation>().Cast<BuildOperation>())
-            {
-                foreach (var foundation in build.Foundations)
-                {
-                    free_foundations.Remove(foundation);
-                }
-            }
-
-            BuildOperation.Foundations.Clear();
-            BuildOperation.Foundations.AddRange(free_foundations);
-
-            var builders = BuildOperation.Units.Count();
-            if (builders < BuildOperation.Foundations.Count && builders < 4)
-            {
-                var free_vill = Unary.OperationsManager.FreeUnits.Where(u => u[ObjectData.CMDID] == (int)CmdId.VILLAGER).FirstOrDefault();
-                if (free_vill != null)
-                {
-                    BuildOperation.AddUnit(free_vill);
-                }
-            }
-
-            if (BuildOperation.Foundations.Count == 0)
-            {
-                BuildOperation.ClearUnits();
             }
         }
     }
--- Unary/Unary.cs
+++ Unary/Unary.cs
@@ -7,31 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unary.Managers;
-using Unary.Operations;
-using Unary.Utils;
 
 namespace Unary
 {
     class Unary : Bot
     {
-        public readonly Mod Mod;
-
         public readonly StrategyManager StrategyManager;
         public readonly EconomyManager EconomyManager;
         public readonly BuildingManager BuildingManager;
-        public readonly OperationsManager OperationsManager;
 
         private readonly List<Command> Commands = new List<Command>();
 
         public Unary() : base()
         {
-            Mod = new Mod();
-            Mod.Load();
-
             StrategyManager = new StrategyManager(this);
             EconomyManager = new EconomyManager(this);
             BuildingManager = new BuildingManager(this);
-            OperationsManager = new OperationsManager(this);
         }
 
         internal void ExecuteCommand(Command command)
@@ -43,17 +34,11 @@ namespace Unary
         {
             Commands.Clear();
 
-            UpdateManagers();
-
-            return Commands;
-        }
-
-        private void UpdateManagers()
-        {
             StrategyManager.Update();
             EconomyManager.Update();
             BuildingManager.Update();
-            OperationsManager.Update();
+
+            return Commands;
         }
     }
 }
